f3857ddd06db301d64d13f9611ebeb44d7cb88f7
Mario <marioeduardo.garciasalazar@gmail.com>
2023-08-22 14:11:52 -0600

    estructura inicial

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+// import { useState, useEffect } from 'react'
+  // import Chip from '@mui/material/Chip';
+// import axios from 'axios'
+
+function App() {
+  
+  
+  return(
+    <>
+      <p>
+        POKEMON
+      </p>
+    </>
+  )
+//   const [pokemon,setPokemon] = useState();
+//   const [nombrePokemon, setNombrePokemon] = useState('');
+//   const pokemonTypes = {
+//     ground:"brown",
+//     water:"blue",
+//     bug:"green",
+//     fire:"red",
+//     flying:"#A9CFFF",
+//     poison:"#BE0ED1"
+//   };
+
+//   const obtenerPokemon = () => {axios.get('https://pokeapi.co/api/v2/pokemon/'+nombrePokemon)
+//   .then(function (response){
+//     console.log(response.data);
+//     setPokemon(response.data)
+//   })
+//   .catch(function (error){
+//     console.log(error);
+//   })
+//   .finally(function(){
+
+//   });
+
+// };
+//   return (
+//     <>
+//     <input placeholder='Ingresa el Nombre del pokemon' onChange={(e)=>setNombrePokemon(e.target.value)}></input>
+//     <button onClick={obtenerPokemon}>Buscar</button>
+//     { pokemon &&
+//       <>
+//         <h1>{pokemon.name}</h1>
+//         <h2>{pokemon.id}</h2>
+//         <img src={pokemon.sprites.front_shiny}></img>
+//         <p>{JSON.stringify(pokemon.types)}</p>
+//         {
+//           pokemon.types.map((x)=>{
+//             return <Chip key={x} label = { x.type.name } sx={{backgroundColor: pokemonTypes[x.type.name]}} variante="outlined" />
+//           })
+//         }
+//       </>
+//     }
+//     </>
+  // )
+}
+export default App
