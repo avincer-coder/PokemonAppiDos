6ac88a1e3f9c1164f48891047f1339ca7babd0ad
Mario <marioeduardo.garciasalazar@gmail.com>
2023-08-26 18:10:18 -0600

    previo a filtrar

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,89 @@
   // import Chip from '@mui/material/Chip';
 // import axios from 'axios'
 
-function App() {
-  
-  
-  return(
-    <>
-      <p>
-        POKEMON
-      </p>
+import "./App.css"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-      <p>
-        Holaaaaaa
-      </p>
-    </>
-  )
+
+
+function App() {
+  const [listaPokemon, setlistaPokemon] = useState([]);
+  const [text, setText] = useState("Hola, mundo!");
+
+  useEffect(() => {
+   const fetchdata = async () =>{
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon/') // PRIMERA APPI
+  
+    setlistaPokemon(response.data.results) // ListaPokemon = primera data
+
+    //Multiples llamadas para obtener URLS
+    //El resultado maps lo guarde en imagen Pokemon(varios objetos)
+      const imagenPokemon = response.data.results.map( async (PokemonID) => {
+        const responseV2 = await axios.get(PokemonID.url)
+        
+        //Creamos un objeto para guardar la URL de la imagen y el pokemon
+        return {
+          url:responseV2.data.sprites.front_default,
+          nombre: responseV2.data.name
+        }
+
+      })
+      //El resultado de imagen pokemon (array de pokemones con url) ahora esta en lista pokemon
+      setlistaPokemon(await Promise.all(imagenPokemon))
+
+      
+    }
+    
+  fetchdata()
+
+      // .catch(error => {
+      //   console.error('Error:', error);
+      // });
+  }, []); 
+
+  //Filtro con valor de input a ListaPokemon
+  //  ----------- B 
+
+    const ListaPokemonFiltrada = listaPokemon.filter(
+    (Pokemon) => Pokemon.nombre = text
+  );
+
+      console.log(ListaPokemonFiltrada)
+      
+  function ProcesoFiltro(e){
+    console.log(e)
+    setText(e.target.value);
+  };
+
+  return( 
+    <div className="contenedor_all">
+      <header>
+        <img src="" alt="" />
+      </header>
+        <h1>Pokedex.org</h1>
+        
+        <input type="text" 
+        onChange={ProcesoFiltro} 
+        value={text} placeholder="Search Pokemon information" />
+        
+        <p>El valor del input es: {text}</p>
+        <main className="contenedor_main">
+          <ul className="contenedor_cards">
+            {listaPokemon.map(Pokemon => (
+            <li className="contenedor_card" key={Pokemon.nombre}>
+              <img src={Pokemon.url} alt="" />
+              <p>{Pokemon.nombre}</p>
+            </li>
+            ))}
+          </ul>
+        </main>
+    </div>
+  );
+}
+
+export default App;
+
 //   const [pokemon,setPokemon] = useState();
 //   const [nombrePokemon, setNombrePokemon] = useState('');
 //   const pokemonTypes = {
@@ -59,5 +128,10 @@ function App() {
 //     }
 //     </>
   // )
-}
-export default App
+
+
+
+// 1. Funccion onchange a input text "e"
+// 2. Guardar el value en variable de input (SE LLAMA text)
+// 3. Crear Use state para modificar variable
+// 4. Filtro con filter() a listapokemon antes del renderizado
---
+++ src/componentes/card/Card.jsx
@@ -0,0 +1,8 @@
+function Card(){
+    return(
+        <>
+            
+        </>
+    )
+}
+export default Card()
